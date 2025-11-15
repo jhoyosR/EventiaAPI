bb5a4337c180155c6713ed5facf93ef61a605096
jhoyosR <jhoyos0410@cue.edu.co>
2025-11-14 21:34:14 -0500

    New: Crea factories para las entidades Participant y Event

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,19 @@
+<?php
+namespace Database\Factories;
+use App\Models\Event\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EventFactory extends Factory
+{
+    protected $model = Event::class;
+    public function definition()
+    {
+        return [
+            'name'=>$this->faker->sentence(3),
+            'description'=>$this->faker->paragraph(),
+            'datetime'=>$this->faker->dateTimeBetween('+1 days','+1 month'),
+            'location'=>$this->faker->city(),
+            'capacity'=>$this->faker->numberBetween(10,500)
+        ];
+    }
+}
---
+++ database/factories/ParticipantFactory.php
@@ -0,0 +1,19 @@
+<?php
+namespace Database\Factories;
+use App\Models\Participant\Participant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ParticipantFactory extends Factory
+{
+    protected $model = Participant::class;
+    public function definition()
+    {
+        return [
+            'name'=>$this->faker->name(),
+            'email'=>$this->faker->unique()->safeEmail(),
+            'phone_number'=>$this->faker->phoneNumber(),
+            'identification'=>$this->faker->unique()->numerify('##########'),
+            'birth_date'=>$this->faker->date()
+        ];
+    }
+}
